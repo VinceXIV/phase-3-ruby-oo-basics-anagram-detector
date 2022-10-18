06af4fddc5fe2fbb16cf64d7acd795822579d31f
Vincent <makokhavomondi@gmail.com>
2022-10-18 18:32:24 +0300

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+    def initialize(word)
+        @char_array = word.chars.sort
+    end
+
+    def match(word_array)
+        anagrams = word_array.filter do |word|
+            @char_array == word.chars.sort
+        end
+
+        anagrams
+    end
+end
